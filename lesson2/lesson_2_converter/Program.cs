d1f4eb37a40ca9ead02a64b70c84595b3011a353
Zeovetar <zeovetar@gmail.com>
2021-10-01 13:22:37 +0300

    Rome converter done

--- lesson2/lesson_2_converter/Program.cs
+++ lesson2/lesson_2_converter/Program.cs
@@ -1,67 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace lesson_2_converter
 {
     class Program
     {
-        [Flags]
-        public enum numbers 
-        { 
-            I = 1,
-            V = 5,
-            X = 10,
-            L = 50,
-            C = 100,
-            D = 500,
-            M = 1000,
-        }
 
-        public enum weigth
-        {
-            I = 0b_0000001,
-            V = 0b_0000011,
-            X = 0b_0000111,
-            L = 0b_0001111,
-            C = 0b_0011111,
-            D = 0b_0111111,
-            M = 0b_1111111,
-        }
         static void Main(string[] args)
         {
+            Dictionary<char, int> rome_symbols = new Dictionary<char, int>(7);
+            rome_symbols.Add('I', 1);
+            rome_symbols.Add('V', 5);
+            rome_symbols.Add('X', 10);
+            rome_symbols.Add('L', 50);
+            rome_symbols.Add('C', 100);
+            rome_symbols.Add('D', 500);
+            rome_symbols.Add('M', 1000);
+
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.InputEncoding = System.Text.Encoding.Unicode;
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Римско-арабский конвертер чисел.");
             Console.ResetColor();
-/*            int counter = 5;
-            while (counter > 3)
+            Console.WriteLine("Введите римское число! К вводу возможны латинские буквы: I, V, X, L, C, D, M");
+            string rome_range = Console.ReadLine();
+            rome_range = rome_range.ToUpper();
+            char[] rome_string = rome_range.ToCharArray();
+            int arabic = 0;
+            for (int i = rome_string.Length - 1; i > 0; i--)
             {
-                counter = 0;*/
-                Console.WriteLine("Введите римское число! К вводу возможны латинские буквы: I, V, X, L, C, D, M");
-                string rome_range = Console.ReadLine();
-                rome_range = rome_range.ToUpper();
-                char[] rome_string = rome_range.ToCharArray();
-            for (int i = rome_string.Length - 1; i >= 0; i--)
-            {
-                Console.WriteLine(rome_string[i]);
-            }
-                //Console.WriteLine((int)weigth.M/(int)weigth.D); //если результат деления 2. и больше\меньшее = 10 то можно вычитать из большего меньшее
-                //Console.WriteLine(weigth.M - weigth.C);
-                /*foreach (char i in rome_range)
+                int result = (rome_symbols[rome_string[i - 1]] / 10);
+                string check = result.ToString();
+                if (i == rome_string.Length - 1)
                 {
-                    if (i == 'V' | i == 'L' | i == 'D')
-                    {
-                        counter++;
-                    }
-                    Console.WriteLine(counter);
+                    arabic = rome_symbols[rome_string[i]];
                 }
-                if (counter > 3) { Console.WriteLine("Буквы V,L,D не могут повторяться! Попробуйте еще раз!"); }*/
-            //}
-            //Console.WriteLine(rome_range.Split("V"));
-/*            if (rome_range.Contains("V"))
-            {
-                
-            }*/
+                if ((arabic > rome_symbols[rome_string[i - 1]]) && (arabic/rome_symbols[rome_string[i - 1]] == 10) && 
+                    (((check[check.Length - 1] == '0') || ((check[check.Length - 1] == '1'))) || (rome_symbols[rome_string[i - 1]] == 1)))
+                {
+                    arabic = arabic - rome_symbols[rome_string[i - 1]];
+                }
+                else
+                {
+                    arabic = arabic + rome_symbols[rome_string[i - 1]];
+                }
+            }
+            Console.WriteLine($"Римское: {rome_range} = Арабское: {arabic}");
         }
     }
 }

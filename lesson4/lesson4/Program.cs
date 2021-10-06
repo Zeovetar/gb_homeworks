eb08aa5dc9b086ac9ff9fb0b4f4dc38c4582e35e
Zeovetar <zeovetar@gmail.com>
2021-10-06 21:13:55 +0300

    first exersize of homework 4

---
+++ lesson4/lesson4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            //list, конечно, сам по себе ref, но че то использовал его, а потом решил понять как правильно по ref передавать, получилась тавтология )))
+            List<string> SomePeople = new List<string>();
+            UserDataDialog(ref SomePeople);
+            foreach (string i in SomePeople)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            string fio = ($"{lastName} {firstName} {patronymic}");
+
+            return fio;
+        }
+
+        static void UserDataDialog(ref List<string>str)
+        {
+            Console.WriteLine("Здравствуйте! Пожалуйста, введите несколько ФИО!\nЕсли надоест вводить, откажитесь, когда программа предложит!");
+            bool stop = true;
+            do
+            {
+                MakeItPretty(ref str, GetNames());
+                Console.WriteLine("Будем вводить следующего? Y\\N");
+                string ans = Console.ReadLine().ToUpper();
+                if (ans == "Н") { stop = false; }
+            } while (stop);
+        }
+
+        static string[] GetNames()
+        {
+            string[] fio = new string[3];
+            Console.WriteLine("Введите имя человека:");
+            fio[0] = Console.ReadLine();
+            Console.WriteLine("Введите фамилию человека:");
+            fio[1] = Console.ReadLine();
+            Console.WriteLine("Введите отчество человека:");
+            fio[2] = Console.ReadLine();
+            return fio;
+        }
+
+        static void MakeItPretty(ref List<string>str1, string[]str2)
+        {
+            str1.Add(GetFullName(str2[0], str2[1], str2[2]));
+        }
+    }
+}

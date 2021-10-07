e83efdde6aa9c7d69f726270d201d860007fa20b
Константин Артозеев <zeovetar@gmail.com>
2021-10-08 01:23:45 +0300

    fix the condition

--- lesson4/lesson4_3/Program.cs
+++ lesson4/lesson4_3/Program.cs
@@ -10,31 +10,46 @@ namespace lesson4_3
         Autumn = 0b011100000000,
         Winter = 0b100000000011
     }
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.InputEncoding = System.Text.Encoding.Unicode;
-            Console.WriteLine(GetSeason(10));
+            while (true)
+            {
+                Console.WriteLine(GetSeason((Convert.ToInt32(Console.ReadLine()))));
+            }
         }
 
         static string GetSeason(int iter)
         {
-            char[] CharToFlag = new char[12];
-            for (int i = 0; i < 12; i++)
-            {
-                CharToFlag[i] = i != iter-1 ? '0' : '1';
-            }
-            Array.Reverse(CharToFlag);
-            string mask = new string(CharToFlag);
+            int mask = rang(iter);
             string set_season = "";
-            int result_mask = Int32.Parse(mask);
             foreach (Season seek in (Season[])Enum.GetValues(typeof(Season)))
             {
-                if (((int)seek & result_mask) != 0) { set_season = seek.ToString(); }
+                if (((int)seek & mask) != 0) { set_season = seek.ToString(); }
             }
             return set_season;
+        }
+
+        static int rang(int iter)
+        {
+            int step = 1;
+            if (iter > 1)
+            {
+                for (int i = 0; i < iter-1; i++)
+                {
+                    step = step * 2;
+                }
+                return step;
+            }
+            else
+            {
+                return step;
+            }
+
         }
     }
 }
